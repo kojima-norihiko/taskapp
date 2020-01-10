e0106c1ed7c0cdfecb10d3460f9d39cda45d1ab6
kojima-norihiko <norihiko.kojima@nex.nikkei.com>
2020-01-10 16:07:48 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by kojima on 2020/01/10.
+//  Copyright © 2020 kojima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
